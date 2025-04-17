912d3a8466b65877e0293b496e912bcf66dcbee4
chandanyadav2004 <demo8788151949@gmail.com>
2025-04-17 14:35:45 +0530

    style fonts

--- index.php
+++ index.php
@@ -8,7 +8,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-
+    <style>
+        *{
+           font-family: 'Poppins', sans-serif; 
+        }
+        .h-font{
+            font-family: 'Merienda', cursive;
+        }
+    </style>
 </head>
 <body>
     
@@ -19,8 +26,6 @@
 
 
 
-    <!-- font-family: 'Poppins', sans-serif;
-    font-family: 'Merienda', cursive; -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
