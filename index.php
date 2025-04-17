86dfd26249d39f2f0fa1858fb5f68b0de8f0828a
chandanyadav2004 <demo8788151949@gmail.com>
2025-04-17 14:46:06 +0530

    NavBar

--- index.php
+++ index.php
@@ -19,6 +19,41 @@
 </head>
 <body>
     
+        <!-- Nav Bar  -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-white ps-lg-3 py-lg-2 shadow-sm sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Chandan Hotel</a>
+    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#">Rooms</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#">Facilities</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#">About Us</a>
+        </li>
+
+        
+      </ul>
+      <div class="d-flex">
+        <button class="btn btn-outline-success " type="submit">Search</button>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
 
 
 
