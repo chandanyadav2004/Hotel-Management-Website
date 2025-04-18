80691762a0eada17f17c51e3b06e3c23a7e8243f
chandanyadav2004 <demo8788151949@gmail.com>
2025-04-18 20:30:06 +0530

    register modal fixing

--- index.php
+++ index.php
@@ -70,28 +70,28 @@
                 </h5>
                 <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-            <div class="mb-3">
-                <label class="form-label">Email address</label>
-                <input type="email" class="form-control shadow-none" >
-            </div>
-            <div class="mb-4">
-                <label class="form-label">Password</label>
-                <input type="password" class="form-control shadow-none" >
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <button type="submit" class="btn btn-dark shadow-none ">Login</button>
-                <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot Password</a>
-            </div>
+              <div class="modal-body">
+                <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control shadow-none" >
+                </div>
+                <div class="mb-4">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control shadow-none" >
+                </div>
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <button type="submit" class="btn btn-dark shadow-none ">Login</button>
+                    <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot Password</a>
+                </div>
+              </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Understood</button>
             </div>
         </form>
-      
+      </div>
     </div>
   </div>
-</div>
 
 
 <!-- Register Modal -->
@@ -105,28 +105,28 @@
                 </h5>
                 <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-            <div class="mb-3">
-                <label class="form-label">Email address</label>
-                <input type="email" class="form-control shadow-none" >
-            </div>
-            <div class="mb-4">
-                <label class="form-label">Password</label>
-                <input type="password" class="form-control shadow-none" >
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <button type="submit" class="btn btn-dark shadow-none ">Login</button>
-                <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot Password</a>
-            </div>
+              <div class="modal-body">
+                <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control shadow-none" >
+                </div>
+                <div class="mb-4">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control shadow-none" >
+                </div>
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <button type="submit" class="btn btn-dark shadow-none ">Login</button>
+                    <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot Password</a>
+                </div>
+              </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Understood</button>
             </div>
         </form>
-      
+      </div>
     </div>
   </div>
-</div>
 
 
 
