f8997cdb7fbb5144142bd416a20a88e5c313a4bb
chandanyadav2004 <demo8788151949@gmail.com>
2025-04-19 11:14:15 +0530

    register modal large size for more adding colulms

--- index.php
+++ index.php
@@ -96,17 +96,18 @@
 
 <!-- Register Modal -->
 <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
         <form action="">
             <div class="modal-header">
                 <h5 class="modal-title d-flex aligns-items-center" >
-                <i class="bi bi-person-circle fs-3 me-2"></i>User Login 
+                <i class="bi bi-person-lines-fill fs-3 me-2"></i>User Register
                 </h5>
                 <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
               <div class="modal-body">
-                <div class="mb-3">
+                
+                <!-- <div class="mb-3">
                     <label class="form-label">Email address</label>
                     <input type="email" class="form-control shadow-none" >
                 </div>
@@ -117,12 +118,8 @@
                 <div class="d-flex align-items-center justify-content-between mb-2">
                     <button type="submit" class="btn btn-dark shadow-none ">Login</button>
                     <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot Password</a>
-                </div>
+                </div> -->
               </div>
-            <!-- <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Understood</button>
-            </div> -->
         </form>
       </div>
     </div>
