7ba62c63c60575bf007af748d49e90c82f663914
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-09 13:20:58 +0530

    Footer Section

--- index.php
+++ index.php
@@ -414,7 +414,7 @@
 
   <!-- Testimonials -->
   <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
-  <div class="container mt-5">
+  <div class="container ">
     <div class="swiper swiper-testimonials">
       <div class="swiper-wrapper mb-5">
 
@@ -533,9 +533,37 @@
       </div>
     </div>
 
-    <br><br><br>
-    <br><br><br>
 
+    <!-- End Reach Us  -->
+
+    <!-- Footer Section -->
+    <div class="container-fluid bg-white mt-5">
+      <div class="row">
+        <div class="col-lg-4 p-4">
+          <h3 class="h-font fw-bold fs-3 mb-2">Chandan Hotel</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Rem molestiae obcaecati odio delectus cum commodi, ipsa iure voluptatum assumenda repellat beatae sapiente perspiciatis aut omnis quae? Molestiae minima nisi vero.
+          </p>
+        </div>
+        <div class="col-lg-4 p-4">
+          <h5 class="mb-3">Links</h5>
+          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Home</a><br>
+          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Rooms</a><br>
+          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Facilities</a><br>
+          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Contact us</a><br>
+          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">About</a><br>
+        </div>
+        <div class="col-lg-4 p-4">
+          <h5 class="mb-3">Follow us</h5>
+          <a href="#" class="d-inline-block text-dark text-decoration-none  mb-2 "><i class="bi bi-twitter me-1"></i>Twitter</a><br>
+          <a href="#" class="d-inline-block text-dark text-decoration-none  mb-2 "><i class="bi bi-facebook me-1"></i>Facebook</a><br>
+          <a href="#" class="d-inline-block text-dark text-decoration-none   "><i class="bi bi-instagram me-1"></i>Instagram</a><br>
+        </div>
+      </div>
+    </div>
+
+    <h6 class="text-center bg-dark  text-white p-3 m-0 ">Designed and Developed by Chandan WEBDEV</h6>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
