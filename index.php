ba6d637f7a851a1e107b06ab09906c7b0b2343d2
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-09 12:03:22 +0530

    Facilities Section

--- index.php
+++ index.php
@@ -381,6 +381,40 @@
 <!-- Ended Our Rooms -->
 
 
+<!-- Our Facilities -->
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
+  <div class="container">
+    <div class="row justify-content-evenly px-lg-0 px-md-0 px-3">
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/ac.svg" width="80px">
+        <h5 class="mt-3">AC</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/tv.svg" width="80px">
+        <h5 class="mt-3">TV</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-12 text-center mt-5">
+        <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none" >More Facilities >>></a>
+      </div>
+    </div>
+  </div>
+<!-- End Our Facilities -->
+
+
+
+
 <br><br><br>
 <br><br><br>
 
