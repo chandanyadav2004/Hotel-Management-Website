3b0912b0502542a5217d378fcdcc3c0266d38edf
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-10 19:22:51 +0530

    link of header and footer pages

--- index.php
+++ index.php
@@ -34,144 +34,7 @@
 
 <body class="bg-light">
   <!-- Header Design -->
-  <!-- Nav Bar  -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-white ps-lg-3 py-lg-2 shadow-sm sticky-top">
-    <div class="container-fluid">
-      <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Chandan Hotel</a>
-      <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link me-2" href="#">Rooms</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link me-2" href="#">Facilities</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link me-2" href="#">Contact Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="#">About Us</a>
-          </li>
-
-
-        </ul>
-        <div class="d-flex">
-          <!-- <button class="btn btn-outline-success " type="submit">Search</button> -->
-          <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal"
-            data-bs-target="#loginModal">Login</button>
-          <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal"
-            data-bs-target="#registerModal">Register</button>
-        </div>
-      </div>
-    </div>
-  </nav>
-  <!-- Login Modal -->
-  <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <form action="">
-          <div class="modal-header">
-            <h5 class="modal-title d-flex aligns-items-center">
-              <i class="bi bi-person-circle fs-3 me-2"></i>User Login
-            </h5>
-            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <div class="mb-3">
-              <label class="form-label">Email address</label>
-              <input type="email" class="form-control shadow-none">
-            </div>
-            <div class="mb-4">
-              <label class="form-label">Password</label>
-              <input type="password" class="form-control shadow-none">
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-              <button type="submit" class="btn btn-dark shadow-none ">Login</button>
-              <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot Password</a>
-            </div>
-          </div>
-          <!-- <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Understood</button>
-            </div> -->
-        </form>
-      </div>
-    </div>
-  </div>
-  <!-- Register Modal -->
-  <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <form action="">
-          <div class="modal-header">
-            <h5 class="modal-title d-flex aligns-items-center">
-              <i class="bi bi-person-lines-fill fs-3 me-2"></i>User Register
-            </h5>
-            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
-              Note: Your details must match with your ID(Aadhar card ,passport, driver license etc.)
-              that you will show at the time of check-in.
-            </span>
-            <div class="container-fluid">
-              <div class="row">
-                <div class="col-md-6 ps-0 mb-3">
-                  <label class="form-label">Name </label>
-                  <input type="text" class="form-control shadow-none">
-                </div>
-                <div class="col-md-6 p-0 mb-3">
-                  <label class="form-label">Email</label>
-                  <input type="email" class="form-control shadow-none">
-                </div>
-                <div class="col-md-6 ps-0 mb-3">
-                  <label class="form-label">Phone Number </label>
-                  <input type="number" class="form-control shadow-none">
-                </div>
-                <div class="col-md-6 p-0 mb-3">
-                  <label class="form-label">Picture</label>
-                  <input type="file" class="form-control shadow-none">
-                </div>
-                <div class="col-md-12 p-0 mb-3">
-                  <label class="form-label">Address</label>
-                  <textarea class="form-control shadow-none" rows="1"></textarea>
-                </div>
-                <div class="col-md-6 ps-0 mb-3">
-                  <label class="form-label">Pincode </label>
-                  <input type="number" class="form-control shadow-none">
-                </div>
-                <div class="col-md-6 p-0 mb-3">
-                  <label class="form-label">Date of birth</label>
-                  <input type="date" class="form-control shadow-none">
-                </div>
-                <div class="col-md-6 ps-0 mb-3">
-                  <label class="form-label">Password </label>
-                  <input type="password" class="form-control shadow-none">
-                </div>
-                <div class="col-md-6 p-0 mb-3">
-                  <label class="form-label">Confirm Password</label>
-                  <input type="password" class="form-control shadow-none">
-                </div>
-              </div>
-            </div>
-            <div class="text-center my-1">
-              <button type="submit" class="btn btn-dark shadow-none ">Register</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
+  <?php require('include/header.php'); ?>
   <!-- End of Header Design -->
   <!-- Carousel Design -->
   <div class="container-fluid px-lg-4 mt-4">
@@ -495,7 +358,9 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
-          <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124040.30875721737!2d81.71918332323344!3d25.402240789395414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398534c9b20bd49f%3A0xa2237856ad4041a!2sPrayagraj%2C%20Uttar%20Pradesh!5e1!3m2!1sen!2sin!4v1746774919219!5m2!1sen!2sin"  loading="lazy" ></iframe>
+          <iframe class="w-100 rounded" height="320px"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124040.30875721737!2d81.71918332323344!3d25.402240789395414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398534c9b20bd49f%3A0xa2237856ad4041a!2sPrayagraj%2C%20Uttar%20Pradesh!5e1!3m2!1sen!2sin!4v1746774919219!5m2!1sen!2sin"
+            loading="lazy"></iframe>
         </div>
         <div class="col-lg-4 col-md-4">
           <div class="bg-white p-4 rounded mb-4">
@@ -537,33 +402,8 @@
     <!-- End Reach Us  -->
 
     <!-- Footer Section -->
-    <div class="container-fluid bg-white mt-5">
-      <div class="row">
-        <div class="col-lg-4 p-4">
-          <h3 class="h-font fw-bold fs-3 mb-2">Chandan Hotel</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Rem molestiae obcaecati odio delectus cum commodi, ipsa iure voluptatum assumenda repellat beatae sapiente perspiciatis aut omnis quae? Molestiae minima nisi vero.
-          </p>
-        </div>
-        <div class="col-lg-4 p-4">
-          <h5 class="mb-3">Links</h5>
-          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Home</a><br>
-          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Rooms</a><br>
-          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Facilities</a><br>
-          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">Contact us</a><br>
-          <a href="#" class="d-inline-block mb-3 text-dark text-decoration-none">About</a><br>
-        </div>
-        <div class="col-lg-4 p-4">
-          <h5 class="mb-3">Follow us</h5>
-          <a href="#" class="d-inline-block text-dark text-decoration-none  mb-2 "><i class="bi bi-twitter me-1"></i>Twitter</a><br>
-          <a href="#" class="d-inline-block text-dark text-decoration-none  mb-2 "><i class="bi bi-facebook me-1"></i>Facebook</a><br>
-          <a href="#" class="d-inline-block text-dark text-decoration-none   "><i class="bi bi-instagram me-1"></i>Instagram</a><br>
-        </div>
-      </div>
-    </div>
-
-    <h6 class="text-center bg-dark  text-white p-3 m-0 ">Designed and Developed by Chandan WEBDEV</h6>
+    <?php require('include/footer.php'); ?>
+    <!-- End Footer Section -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
