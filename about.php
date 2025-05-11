63a077a5eefa0c992e28b3d415c86787de76b7ef
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-11 18:15:39 +0530

    About us heading

--- about.php
+++ about.php
@@ -17,6 +17,17 @@
 <?php require('include/header.php'); ?>
 <!-- End of Header Design -->
 
+<div class="my-5 px-4">
+  <h2 class="fw-bold h-font text-center">ABOUT US </h2>
+  <div class="h-line bg-dark"></div>
+  <p class="text-center mt-3">
+    Lorem ipsum dolor sit amet consectetur, 
+    adipisicing elit. <br> Provident sapiente molestias 
+    fugit accusantium incidunt nulla vero inventore eum neque dolore.
+
+  </p>
+</div>
+
 
 
 
