53cf4493366de7a8ba70a5198a983f95ef3682f6
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-11 18:59:31 +0530

    management team finish

--- about.php
+++ about.php
@@ -6,7 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hotel Management Website - About </title>
   <?php require('include/links.php'); ?>
-  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+  <style>
+    .box {
+      border-top-color: var(--teal) !important;
+    }
+  </style>
 
   
   
@@ -46,6 +51,85 @@
 </div>
 
 
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-lg-3 col-md-6 mb-4 px-4">
+      <div class="bg-white shadow rounded p-4 text-center border-top border-4 box">
+        <img src="images/about/hotel.svg" width="70px">
+        <h4 class="mt-3">100+ ROOMS</h4>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 mb-4 px-4">
+      <div class="bg-white shadow rounded p-4 text-center border-top border-4 box">
+        <img src="images/about/customers.svg" width="70px">
+        <h4 class="mt-3">200+ CUSTOMER</h4>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 mb-4 px-4">
+      <div class="bg-white shadow rounded p-4 text-center border-top border-4 box">
+        <img src="images/about/rating.svg" width="70px">
+        <h4 class="mt-3">150+ REVIEWS</h4>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 mb-4 px-4">
+      <div class="bg-white shadow rounded p-4 text-center border-top border-4 box">
+        <img src="images/about/staff.svg" width="70px">
+        <h4 class="mt-3">200+ STAFF</h4>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<h3 class="my-5 fw-bold h-font text-center ">MANAGEMENT TEAM</h3>
+
+<div class="container px-4">
+    <div class="swiper mySwiper ">
+      <div class="swiper-wrapper mb-5">
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 1</h5>
+        </div>
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 2</h5>
+        </div>
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 3</h5>
+        </div>
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 4</h5>
+        </div>
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 5</h5>
+        </div>
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 6</h5>
+        </div>
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 7</h5>
+        </div>
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 8</h5>
+        </div>
+        <div class="swiper-slide bg-white rounded text-center overflow-hidden">
+          <img src="images/about/team.jpg" class="w-100">
+          <h5 class="mt-2">Random Name 9 </h5>
+        </div>
+
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+</div>
 
 <!-- Footer Section -->
 <?php require('include/footer.php'); ?>
@@ -53,8 +137,35 @@
 
     
 
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    
+<!-- Swiper JS -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+  <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+          320: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }
+    });
+  </script>
+
+
 </body>
 
 </html>
