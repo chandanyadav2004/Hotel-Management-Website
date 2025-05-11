03b97a61bf34f3f486093421d319fca1c1e89d5d
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-11 18:13:30 +0530

    About us page adding

---
+++ about.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hotel Management Website - About </title>
+  <?php require('include/links.php'); ?>
+  
+
+  
+  <style>
+    .h-line {
+      width: 150px;
+      margin: 0 auto;
+      height: 1.7px;
+    }
+    .pop:hover{
+      border-top-color: var(--teal) !important;
+      transform: scale(1.05);
+      transition: all 0.3s;
+    }
+  </style>
+</head>
+
+<body class="bg-light">
+<!-- Header Design -->
+<?php require('include/header.php'); ?>
+<!-- End of Header Design -->
+
+<div class="my-5 px-4">
+  <h2 class="fw-bold h-font text-center">ABOUT US </h2>
+  <div class="h-line bg-dark"></div>
+  <p class="text-center mt-3">
+    Lorem ipsum dolor sit amet consectetur, 
+    adipisicing elit. <br> Provident sapiente molestias 
+    fugit accusantium incidunt nulla vero inventore eum neque dolore.
+
+  </p>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/wifi.svg" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/ac.svg" width="40px">
+          <h5 class="m-0 ms-3">AC</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/tv.svg" width="40px">
+          <h5 class="m-0 ms-3">TV</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/wifi.svg" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/wifi.svg" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/wifi.svg" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+<!-- Footer Section -->
+<?php require('include/footer.php'); ?>
+<!-- End Footer Section -->
+
+    
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    
+</body>
+
+</html>
