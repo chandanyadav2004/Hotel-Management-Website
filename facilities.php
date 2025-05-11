8eba31bd6e1c09ee904d26c6ae2c96acb7ea360f
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-11 18:06:57 +0530

    facilities page work done

--- facilities.php
+++ facilities.php
@@ -17,18 +17,10 @@
       margin: 0 auto;
       height: 1.7px;
     }
-    .availability-form {
-      margin-top: -50px;
-      z-index: 2;
-      position: relative;
-    }
-
-    @media screen and (max-width: 575px) {
-      .availability-form {
-        margin-top: 25px;
-        padding: 0 35px;
-      }
-
+    .pop:hover{
+      border-top-color: var(--teal) !important;
+      transform: scale(1.05);
+      transition: all 0.3s;
     }
   </style>
 </head>
@@ -41,7 +33,103 @@
 <div class="my-5 px-4">
   <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
   <div class="h-line bg-dark"></div>
+  <p class="text-center mt-3">
+    Lorem ipsum dolor sit amet consectetur, 
+    adipisicing elit. <br> Provident sapiente molestias 
+    fugit accusantium incidunt nulla vero inventore eum neque dolore.
+
+  </p>
 </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/wifi.svg" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/ac.svg" width="40px">
+          <h5 class="m-0 ms-3">AC</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/tv.svg" width="40px">
+          <h5 class="m-0 ms-3">TV</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/wifi.svg" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/wifi.svg" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+          <img src="images/facilities/wifi.svg" width="40px">
+          <h5 class="m-0 ms-3">Wifi</h5>
+        </div>
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptate officia, dolor ad praesentium alias nostrum hic!
+        </p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
 
 <!-- Footer Section -->
 <?php require('include/footer.php'); ?>
