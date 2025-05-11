a55e59350334c8c2a191695d0c8f20c8e185141c
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-11 18:14:17 +0530

    unnecassry content removing

--- about.php
+++ about.php
@@ -9,18 +9,7 @@
   
 
   
-  <style>
-    .h-line {
-      width: 150px;
-      margin: 0 auto;
-      height: 1.7px;
-    }
-    .pop:hover{
-      border-top-color: var(--teal) !important;
-      transform: scale(1.05);
-      transition: all 0.3s;
-    }
-  </style>
+  
 </head>
 
 <body class="bg-light">
@@ -28,104 +17,7 @@
 <?php require('include/header.php'); ?>
 <!-- End of Header Design -->
 
-<div class="my-5 px-4">
-  <h2 class="fw-bold h-font text-center">ABOUT US </h2>
-  <div class="h-line bg-dark"></div>
-  <p class="text-center mt-3">
-    Lorem ipsum dolor sit amet consectetur, 
-    adipisicing elit. <br> Provident sapiente molestias 
-    fugit accusantium incidunt nulla vero inventore eum neque dolore.
 
-  </p>
-</div>
-
-<div class="container">
-  <div class="row">
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">Wifi</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/ac.svg" width="40px">
-          <h5 class="m-0 ms-3">AC</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/tv.svg" width="40px">
-          <h5 class="m-0 ms-3">TV</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">Wifi</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">Wifi</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">Wifi</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-
-  </div>
-</div>
 
 
 
