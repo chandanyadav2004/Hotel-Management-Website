a78c46a6c0c3c7ce8401d88c7dd794e1869504b5
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-12 17:43:07 +0530

    IFRAME TAG ADDING

--- contact.php
+++ contact.php
@@ -14,11 +14,7 @@
       margin: 0 auto;
       height: 1.7px;
     }
-    .pop:hover{
-      border-top-color: var(--teal) !important;
-      transform: scale(1.05);
-      transition: all 0.3s;
-    }
+    
   </style>
 </head>
 
@@ -28,7 +24,7 @@
 <!-- End of Header Design -->
 
 <div class="my-5 px-4">
-  <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+  <h2 class="fw-bold h-font text-center">CONTACT US </h2>
   <div class="h-line bg-dark"></div>
   <p class="text-center mt-3">
     Lorem ipsum dolor sit amet consectetur, 
@@ -40,20 +36,14 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">Wifi</h5>
-        </div>
+    <div class="col-lg-6 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shadow p-4 ">
+        <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124040.30875721737!2d81.71918332323344!3d25.402240789395414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398534c9b20bd49f%3A0xa2237856ad4041a!2sPrayagraj%2C%20Uttar%20Pradesh!5e1!3m2!1sen!2sin!4v1746774919219!5m2!1sen!2sin" loading="lazy"></iframe>
         
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
+       
       </div>
     </div>
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
+    <div class="col-lg-6 col-md-6 mb-5 px-4">
       <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
         <div class="d-flex align-items-center mb-2">
           <img src="images/facilities/ac.svg" width="40px">
@@ -67,61 +57,7 @@
       </div>
     </div>
 
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/tv.svg" width="40px">
-          <h5 class="m-0 ms-3">TV</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">Wifi</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">Wifi</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align-items-center mb-2">
-          <img src="images/facilities/wifi.svg" width="40px">
-          <h5 class="m-0 ms-3">Wifi</h5>
-        </div>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Voluptate officia, dolor ad praesentium alias nostrum hic!
-        </p>
-      </div>
-    </div>
+    
 
   </div>
 </div>
