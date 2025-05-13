1ca5cb1026e21235932a14dc40f7a95cdb310347
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-13 19:23:24 +0530

    cost and buttons added

--- rooms.php
+++ rooms.php
@@ -92,10 +92,10 @@
       <div class="col-lg-9 col-md-12 px-4">
         <div class="card mb-4  border-0 shadow">
           <div class="row g-0 p-3 align-items-center">
-            <div class="col-md-5">
+            <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
               <img src="images/rooms/1.jpg" class="img-fluid rounded" alt="...">
             </div>
-            <div class="col-md-5">
+            <div class="col-md-5 px-lg-3 px-md-3 px-0">
               <h5 class="mb-3">Simple Room Name</h5>
               <div class="features mb-3">
                 <h6 class="mb-1">Features</h6>
@@ -112,18 +112,15 @@
                 <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">Free Parking</span>
               </div>
               <div class="guests mb-3">
-              <h6 class="mb-1">Guest</h6>
-              <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">5 Adult</span>
-              <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">4 Children</span>
-            </div>
-            </div>
-            <div class="col-md-2">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h6 class="mb-1">Guest</h6>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">5 Adult</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">4 Children</span>
               </div>
+            </div>
+            <div class="col-md-2 text-center">
+              <h6 class="mb-4">₹200 per night</h6>
+              <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
+              <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
             </div>
           </div>
         </div>
