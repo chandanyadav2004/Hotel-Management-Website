09444bb2c596b24d47700c9c120de38a45a0c501
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-13 19:26:26 +0530

    Card 3

--- rooms.php
+++ rooms.php
@@ -158,6 +158,40 @@
             </div>
           </div>
         </div>
+        <div class="card mb-4  border-0 shadow">
+          <div class="row g-0 p-3 align-items-center">
+            <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+              <img src="images/rooms/1.jpg" class="img-fluid rounded" alt="...">
+            </div>
+            <div class="col-md-5 px-lg-3 px-md-3 px-0">
+              <h5 class="mb-3">Simple Room Name</h5>
+              <div class="features mb-3">
+                <h6 class="mb-1">Features</h6>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">2 Rooms</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">1 Bathroom</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">1 Balcony</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">3 Sofa</span>
+              </div>
+              <div class="facilities mb-3">
+                <h6 class="mb-1">Facilities</h6>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">AC</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">TV</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">Free Wi-Fi</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">Free Parking</span>
+              </div>
+              <div class="guests">
+                <h6 class="mb-1">Guest</h6>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">5 Adult</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">4 Children</span>
+              </div>
+            </div>
+            <div class="col-md-2 text-center">
+              <h6 class="mb-4">₹200 per night</h6>
+              <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
+              <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
+            </div>
+          </div>
+        </div>
       </div>
 
 
