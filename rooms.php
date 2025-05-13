ee95ae24be30fad6552079e9c8618c4f163b4f8c
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-13 19:13:35 +0530

    image and facility added

--- rooms.php
+++ rooms.php
@@ -34,8 +34,9 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
           <div class="container-fluid flex-lg-column align-items-stretch">
             <h4 class="mt-2">FILITERS</h4>
-            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
+              data-bs-target="#filterDropdown" aria-controls="navbarNav" aria-expanded="false"
+              aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse flex-column mt-2 align-items-stretch" id="filterDropdown">
@@ -88,6 +89,33 @@
         </nav>
       </div>
 
+      <div class="col-lg-9 col-md-12 px-4">
+        <div class="card mb-4  border-0 shadow">
+          <div class="row g-0 p-3 align-items-center">
+            <div class="col-md-5">
+              <img src="images/rooms/1.jpg" class="img-fluid rounded" alt="...">
+            </div>
+            <div class="col-md-5">
+              <h5 class="mb-3">Simple Room Name</h5>
+              <div class="features mb-3">
+                <h6 class="mb-1">Features</h6>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">2 Rooms</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">1 Bathroom</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">1 Balcony</span>
+                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">3 Sofa</span>
+              </div>
+            </div>
+            <div class="col-md-2">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                  content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
 
