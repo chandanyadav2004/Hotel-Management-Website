ea2690fef50def43dbe78a5d9369d74cbf2ebfca
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-13 18:50:03 +0530

    Guests section added in filiters

--- rooms.php
+++ rooms.php
@@ -41,9 +41,9 @@
             <div class="collapse navbar-collapse flex-column mt-2 align-items-stretch" id="filterDropdown">
               <div class="border bg-light p-3 rounded mb-3">
                 <h5 class="mb-3" style="font-size:18px;">CHECK AVAILIABILITY</h5>
-                <label class="form-label" >Check-In</label>
+                <label class="form-label">Check-In</label>
                 <input type="date" class="form-control shadow-none mb-3">
-                <label class="form-label" >Check-Out</label>
+                <label class="form-label">Check-Out</label>
                 <input type="date" class="form-control shadow-none">
               </div>
 
@@ -51,23 +51,38 @@
                 <h5 class="mb-3" style="font-size:18px;">FACILITIES</h5>
                 <div class="mb-2">
                   <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
-                  <label class="form-label" for="f1" >Facility one</label>
-                 
+                  <label class="form-label" for="f1">Facility one</label>
+
                 </div>
                 <div class="mb-2">
                   <input type="checkbox" id="f2" class="form-check-input shadow-none me-1">
-                  <label class="form-label" for="f2" >Facility Two</label>
-                 
+                  <label class="form-label" for="f2">Facility Two</label>
+
                 </div>
                 <div class="mb-2">
                   <input type="checkbox" id="f3" class="form-check-input shadow-none me-1">
-                  <label class="form-label" for="f3" >Facility Three</label>
-                 
+                  <label class="form-label" for="f3">Facility Three</label>
+
                 </div>
-                
+
               </div>
 
-              
+              <div class="border bg-light p-3 rounded mb-3">
+                <h5 class="mb-3" style="font-size:18px;">GUESTS</h5>
+                <div class="d-flex">
+                  <div class="me-2">
+                    <label class="form-label">Adults</label>
+                    <input type="number" class="form-control shadow-none">
+                  </div>
+                  <div>
+                    <label class="form-label">Childern</label>
+                    <input type="number" class="form-control shadow-none">
+                  </div>
+                </div>
+
+              </div>
+
+
             </div>
           </div>
         </nav>
