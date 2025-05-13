9cd4a19a37a9806d3f905f3334454d17ffe9aff7
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-13 18:38:16 +0530

    Check availiability filter added

--- rooms.php
+++ rooms.php
@@ -30,7 +30,36 @@
 
   <div class="container">
     <div class="row">
+      <div class="col-lg-3">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
+          <div class="container-fluid flex-lg-column align-items-stretch">
+            <h4 class="mt-2">FILITERS</h4>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse flex-column mt-2 align-items-stretch" id="filterDropdown">
+              <div class="border bg-light p-3 rounded mb-3">
+                <h5 class="mb-3" style="font-size:18px;">CHECK AVAILIABILITY</h5>
+                <label class="form-label" >Check-In</label>
+                <input type="date" class="form-control shadow-none mb-3">
+                <label class="form-label" >Check-Out</label>
+                <input type="date" class="form-control shadow-none">
+              </div>
 
+              <div class="border bg-light p-3 rounded mb-3">
+                <h5 class="mb-3" style="font-size:18px;">CHECK AVAILIABILITY</h5>
+                <label class="form-label" >Check-In</label>
+                <input type="date" class="form-control shadow-none mb-3">
+                <label class="form-label" >Check-Out</label>
+                <input type="date" class="form-control shadow-none">
+              </div>
+
+              
+            </div>
+          </div>
+        </nav>
+      </div>
 
 
 
