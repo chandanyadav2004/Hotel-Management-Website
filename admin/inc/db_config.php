09edea68d180a9bd82bd40a8e2047b4e40b7ccbd
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-14 14:39:43 +0530

    database connection

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -1,3 +1,19 @@
 <!-- Database name is hbwebsite -->
- <!-- Table name  admin_cred -->
+<!-- Table name  admin_cred -->
 <!-- Three coloumns 1. sr_no 2. admin_name 3. admin_pass -->
+
+<?php
+// Database connection
+$hname = "localhost";
+$uname = "root";
+$pass = '';
+// Database name
+$db = "hbwebsite";
+
+$con = mysqli_connect($hname, $uname, $pass, $db);
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
