8c7e9ea74e4bf1a3f534db547971a49b2c9827ee
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-14 14:40:29 +0530

    linking of database

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,10 @@
+<?php 
+    require('inc/db_config.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
