f9ecf364c0f4c173d2709f72627bd9eb7c7cfe6a
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-14 16:30:40 +0530

    login and logout completed fully

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -16,4 +16,36 @@ if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+
+function filteration($data)
+{
+    foreach ($data as $key => $value) {
+        $data[$key] = strip_tags(htmlspecialchars(stripslashes(trim($value))));
+        // $data[$key] = strip_tags($value);
+    }
+    return $data;
+}
+
+function select($sql, $values, $datatype)
+{
+    $con = $GLOBALS['con'];
+    if ($stmt = mysqli_prepare($con, $sql)) {
+        mysqli_stmt_bind_param($stmt, $datatype, ...$values);
+        if (mysqli_stmt_execute($stmt)) {
+            $res = mysqli_stmt_get_result($stmt);
+            mysqli_stmt_close($stmt);
+            return $res;
+        } else {
+            mysqli_stmt_close($stmt);
+            die("Query cannot be executed: select" . mysqli_error($con));
+        }
+
+
+    } else {
+        die("Query cannot be prepared: Select" . mysqli_error($con));
+    }
+}
+
+
+
 ?>
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php
+require('inc/essentials.php');
 require('inc/db_config.php');
 
 ?>
@@ -25,25 +26,50 @@ require('inc/db_config.php');
 <body class="bg-light">
 
     <div class="login-form text-center shadow overflow-hidden bg-white rounded">
-        <form action="">
+        <form method="POST">
             <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
             <div class="p-4">
                 <div class="mb-3">
-                    <input name="admin_name" type="text" class="form-control shadow-none text-center"
+                    <input name="admin_name" required type="text" class="form-control shadow-none text-center"
                         placeholder="Admin Name">
                 </div>
                 <div class="mb-4">
-                    <input name="admin_pass" type="password" class="form-control shadow-none text-center"
+                    <input name="admin_pass" required type="password" class="form-control shadow-none text-center"
                         placeholder="Password">
                 </div>
-                <button  type="submit" class="btn login text-white custom-bg shadow-none">LOGIN</button>
+                <button name="login" type="submit" class="btn  text-white custom-bg shadow-none">LOGIN</button>
             </div>
         </form>
     </div>
 
 
 
+    <?php
 
+    if (isset($_POST['login'])) {
+        $frm_data = filteration($_POST);
+
+        $query = "SELECT * FROM `admin_cred` WHERE `admin_name` = ? AND `admin_pass` = ?";
+        $values = [$frm_data['admin_name'], $frm_data['admin_pass']];
+        $datatypes = "ss";
+
+        $res = select($query, $values, $datatypes);
+
+        if ($res->num_rows == 1) {
+            $row = mysqli_fetch_assoc($res);
+            session_start();
+            $_SESSION["admin_id"] = $row["sr_no"];
+            $_SESSION['admin_login'] = true;
+            redirect('dashboard.php');
+
+        } else {
+            alert('error', 'Invalid Credentials');
+        }
+        // print_r($res);
+    
+    }
+
+    ?>
 
 
 
