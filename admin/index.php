0198996bdc6c4f8ca9643782ac8454b5a9c632df
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-14 14:19:03 +0530

    admin login form added

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chandan Yadav Hotel - Admin Panel </title>
+    <?php require('inc/links.php') ?>
+    <style>
+        div.login-form{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 400px;
+        }
+    </style>
 </head>
-<body>
-    
+
+<body class="bg-light">
+
+    <div class="login-form text-center shadow overflow-hidden bg-white rounded">
+        <form action="">
+            <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
+            <div class="p-4">
+                <div class="mb-3">
+                    <input name="admin_name" type="email" class="form-control shadow-none text-center" placeholder="Admin Name">
+                </div>
+                <div class="mb-4">
+                    <input name="admin_pass" type="password" class="form-control shadow-none text-center" placeholder="Password">
+                </div>
+                <button class="login" type="submit" class="btn text-white custom-bg shadow-none">LOGIN</button>
+            </div>
+        </form>
+    </div>
+
+
+
+
+
+
+
+
+    <?php require('inc/scripts.php'); ?>
 </body>
+
 </html>
