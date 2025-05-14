f271aba759a6f3979ac5a4d548f8cdf46ffdcd71
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-14 14:44:04 +0530

    fixing button

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,18 @@
-<?php 
-    require('inc/db_config.php');
+<?php
+require('inc/db_config.php');
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chandan Yadav Hotel - Admin Panel </title>
     <?php require('inc/links.php') ?>
     <style>
-        div.login-form{
+        div.login-form {
             position: absolute;
             top: 50%;
             left: 50%;
@@ -28,12 +29,14 @@
             <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
             <div class="p-4">
                 <div class="mb-3">
-                    <input name="admin_name" type="email" class="form-control shadow-none text-center" placeholder="Admin Name">
+                    <input name="admin_name" type="text" class="form-control shadow-none text-center"
+                        placeholder="Admin Name">
                 </div>
                 <div class="mb-4">
-                    <input name="admin_pass" type="password" class="form-control shadow-none text-center" placeholder="Password">
+                    <input name="admin_pass" type="password" class="form-control shadow-none text-center"
+                        placeholder="Password">
                 </div>
-                <button class="login" type="submit" class="btn text-white custom-bg shadow-none">LOGIN</button>
+                <button  type="submit" class="btn login text-white custom-bg shadow-none">LOGIN</button>
             </div>
         </form>
     </div>
