3019932522f7f1063e7ef8c17971843cf4ef5fde
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-17 10:52:13 +0530

    team management on setting page work complete

--- admin/setting.php
+++ admin/setting.php
@@ -258,7 +258,18 @@ adminLogin();
                         </div>
 
                         <div class="row" id="team-data">
-
+                            <!-- <div class="col-md-2 mb-3">
+                                <div class="card bg-dark text-white">
+                                    <img src="../images/about/team.jpg" class="card-img" >
+                                    <div class="card-img-overlay text-end">
+                                        <button class="btn btn-danger btn-sm shadow-none float-end" type="button">
+                                            <i class="bi bi-trash"></i>Delete
+                                        </button>
+                                        
+                                    </div>
+                                    <p class="card-text text-center px-3 py-2">Random Name</p>
+                                </div>
+                            </div> -->
                         </div>
                     </div>
                 </div>
@@ -286,7 +297,7 @@ adminLogin();
                                     </div>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" onclick="" class="btn shadow-none text-secondary"
+                                    <button type="button" onclick="member_name.value='',member_picture.value=''" class="btn shadow-none text-secondary"
                                         data-bs-dismiss="modal">Cancel</button>
                                     <button type="submit" class="btn custom-bg text-white shadow-none">Submit</button>
                                 </div>
@@ -476,32 +487,72 @@ adminLogin();
 
         function add_member() {
             let form_data = new FormData();
-            form_data.append('member_name', member_name_inp.value);
+            form_data.append('name', member_name_inp.value);
             let picture = member_picture_inp.files[0];
-            form_data.append('member_picture', picture);
-            form_data.append('add_member', true);
+            form_data.append('picture', picture);
+            form_data.append('add_member', '');
 
             let xhr = new XMLHttpRequest();
             xhr.open('POST', 'ajax/settings_crud.php', true);
 
             xhr.onload = function () {
+                // console.log(this.response);
                 var myModal = document.getElementById('team-s')
                 var modal = bootstrap.Modal.getInstance(myModal) // Returns a Bootstrap modal instance
                 modal.hide() // Hide the modal
-                if (this.response == 1) {
-                    get_team();
-                    alert('success', 'Member added successfully');
+                if (this.responseText == 'inv_img') {
+                    alert('error', 'Only jpg, png and webp image are allowed');
+                } else if (this.responseText == 'inv_size') {
+                    alert('error', 'Image size should be less than 2MB');
+                } else if (this.responseText == 'upd_failed') {
+                    alert('error', 'Image upload failed');
                 } else {
-                    alert('error', 'Member not added successfully');
+                    alert('success', 'Member added successfully');
+                    member_name_inp.value = '';
+                    member_picture_inp.value = '';
+                    get_members();
+                    
                 }
             }
 
             xhr.send(form_data);
         }
 
+        function get_members(){
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', 'ajax/settings_crud.php', true);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function () {
+                // console.log(this.response);
+                document.getElementById('team-data').innerHTML = this.responseText;
+            }
+
+            xhr.send('get_members');
+        }
+
+        function rem_member(id){
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', 'ajax/settings_crud.php', true);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function () {
+                // console.log(this.response);
+                if (this.responseText == 1) {
+                    alert('success', 'Member deleted successfully');
+                    get_members();
+                } else {
+                    alert('error', 'Member not deleted successfully');
+                }
+            }
+
+            xhr.send('rem_member='+id);
+        }
+
         window.onload = function () {
             get_general();
             get_contacts();
+            get_members();
         }
 
     </script>
