3057f83fafcea876e8c387e1ddb887db18c521f6
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-18 12:01:34 +0530

    spacing adjusting in index page

--- index.php
+++ index.php
@@ -94,11 +94,11 @@
             <div class="col-lg-1 mb-lg-3 mt-2">
               <button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
             </div>
-          </div>
-        </form>
       </div>
-      
+      </form>
     </div>
+
+  </div>
   </div>
 
 
@@ -355,117 +355,114 @@
           </div>
 
         </div>
-
-
-
-
       </div>
       <div class="swiper-pagination"></div>
     </div>
+  </div>
 
-    <!-- End Testimonials -->
+  <!-- End Testimonials -->
 
-    <!-- Reach Us  -->
-    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
-          <iframe class="w-100 rounded" height="320px"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124040.30875721737!2d81.71918332323344!3d25.402240789395414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398534c9b20bd49f%3A0xa2237856ad4041a!2sPrayagraj%2C%20Uttar%20Pradesh!5e1!3m2!1sen!2sin!4v1746774919219!5m2!1sen!2sin"
-            loading="lazy"></iframe>
+  <!-- Reach Us  -->
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+        <iframe class="w-100 rounded" height="320px"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124040.30875721737!2d81.71918332323344!3d25.402240789395414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398534c9b20bd49f%3A0xa2237856ad4041a!2sPrayagraj%2C%20Uttar%20Pradesh!5e1!3m2!1sen!2sin!4v1746774919219!5m2!1sen!2sin"
+          loading="lazy"></iframe>
+      </div>
+      <div class="col-lg-4 col-md-4">
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Call us</h5>
+          <a href="tel: +917778889991" class="d-inline-block mb-2 text-decoration-none text-dark ">
+            <i class="bi bi-telephone-fill"></i>+917778889991
+          </a>
+          <br>
+          <a href="tel: +917778889991" class="d-inline-block  text-decoration-none text-dark ">
+            <i class="bi bi-telephone-fill"></i>+917778889991
+          </a>
         </div>
-        <div class="col-lg-4 col-md-4">
-          <div class="bg-white p-4 rounded mb-4">
-            <h5>Call us</h5>
-            <a href="tel: +917778889991" class="d-inline-block mb-2 text-decoration-none text-dark ">
-              <i class="bi bi-telephone-fill"></i>+917778889991
-            </a>
-            <br>
-            <a href="tel: +917778889991" class="d-inline-block  text-decoration-none text-dark ">
-              <i class="bi bi-telephone-fill"></i>+917778889991
-            </a>
-          </div>
-          <div class="bg-white p-4 rounded mb-4">
-            <h5>Follow us</h5>
-            <a href="#" class="d-inline-block mb-3 ">
-              <span class="badge fs-6 p-2 bg-light text-dark">
-                <i class="bi bi-twitter me-1"></i>Twitter
-              </span>
-            </a>
-            <br>
-            <a href="#" class="d-inline-block mb-3 ">
-              <span class="badge fs-6 p-2 bg-light text-dark">
-                <i class="bi bi-facebook me-1"></i>Facebook
-              </span>
-            </a>
-            <br>
-            <a href="#" class="d-inline-block  ">
-              <span class="badge fs-6 p-2 bg-light text-dark">
-                <i class="bi bi-instagram me-1"></i>Instagram
-              </span>
-            </a>
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Follow us</h5>
+          <a href="#" class="d-inline-block mb-3 ">
+            <span class="badge fs-6 p-2 bg-light text-dark">
+              <i class="bi bi-twitter me-1"></i>Twitter
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block mb-3 ">
+            <span class="badge fs-6 p-2 bg-light text-dark">
+              <i class="bi bi-facebook me-1"></i>Facebook
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block  ">
+            <span class="badge fs-6 p-2 bg-light text-dark">
+              <i class="bi bi-instagram me-1"></i>Instagram
+            </span>
+          </a>
 
-          </div>
         </div>
       </div>
     </div>
+  </div>
 
 
-    <!-- End Reach Us  -->
+  <!-- End Reach Us  -->
 
-    <!-- Footer Section -->
-    <?php require('include/footer.php'); ?>
-    <!-- End Footer Section -->
+  <!-- Footer Section -->
+  <?php require('include/footer.php'); ?>
+  <!-- End Footer Section -->
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <!-- Initialize Swiper -->
-    <script>
-      var swiper = new Swiper(".swiper-container", {
-        spaceBetween: 30,
-        effect: "fade",
-        loop: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper(".swiper-container", {
+      spaceBetween: 30,
+      effect: "fade",
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
+
+    var swiper = new Swiper(".swiper-testimonials", {
+      effect: "coverflow",
+      loop: true,
+      grabCursor: true,
+      centeredSlides: true,
+      // slidesPerView: "auto",
+      slidesPerView: "3",
+
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
         },
-      });
-
-      var swiper = new Swiper(".swiper-testimonials", {
-        effect: "coverflow",
-        loop: true,
-        grabCursor: true,
-        centeredSlides: true,
-        // slidesPerView: "auto",
-        slidesPerView: "3",
-
-        coverflowEffect: {
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
+        640: {
+          slidesPerView: 1,
         },
-        pagination: {
-          el: ".swiper-pagination",
+        768: {
+          slidesPerView: 2,
         },
-        breakpoints: {
-          320: {
-            slidesPerView: 1,
-          },
-          640: {
-            slidesPerView: 1,
-          },
-          768: {
-            slidesPerView: 2,
-          },
-          1024: {
-            slidesPerView: 3,
-          },
-        }
-      });
-    </script>
+        1024: {
+          slidesPerView: 3,
+        },
+      }
+    });
+  </script>
 </body>
 
 </html>
