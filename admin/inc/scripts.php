1220b34ee523f526a94e51796eec9bad7a38188c
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-18 14:47:43 +0530

    scrip dynamic

--- admin/inc/scripts.php
+++ admin/inc/scripts.php
@@ -16,3 +16,23 @@
             document.body.appendChild(element);
       }     
 </script>
+
+<script>
+  function setActive() {
+    let navbar = document.getElementById('dashboard-menu');
+    let a_tags = navbar.getElementsByTagName('a');
+
+    for (let i = 0; i < a_tags.length; i++) {
+      // let current_url = window.location.href;
+      let link_url = a_tags[i].href.split('/').pop();
+      let file_name = link_url.split('.')[0];
+
+      if (document.location.href.indexOf(file_name) >=0) {
+        a_tags[i].classList.add('active');
+      } else {
+        a_tags[i].classList.remove('active');
+      }
+    }       
+  }
+  window.onload = setActive;
+</script>
