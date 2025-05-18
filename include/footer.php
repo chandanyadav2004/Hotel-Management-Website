8e4993538895d6a4dec25cad907b09bf908628d1
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-18 14:47:56 +0530

    footer dynamic

--- include/footer.php
+++ include/footer.php
@@ -69,3 +69,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
       crossorigin="anonymous"></script>
+
+<script>
+  function setActive() {
+    let navbar = document.getElementById('nav-bar');
+    let a_tags = navbar.getElementsByTagName('a');
+
+    for (let i = 0; i < a_tags.length; i++) {
+      // let current_url = window.location.href;
+      let link_url = a_tags[i].href.split('/').pop();
+      let file_name = link_url.split('.')[0];
+
+      if (document.location.href.indexOf(file_name) >=0) {
+        a_tags[i].classList.add('active');
+      } else {
+        a_tags[i].classList.remove('active');
+      }
+    }       
+  }
+  window.onload = setActive;
+</script>
