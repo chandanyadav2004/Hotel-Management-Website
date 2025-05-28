42a6923ab9584d95a2167681e013918cf02b324b
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-28 14:37:23 +0530

    path adding rooms

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -5,6 +5,7 @@ define('SITE_URL', 'http://127.0.0.1/Hotel-Management-Website/');
 define('ABOUT_IMG_PATH', SITE_URL . 'images/about/');
 define('CAROUSEL_IMG_PATH', SITE_URL . 'images/carousel/');
 define('FACILITIES_IMG_PATH', SITE_URL . 'images/facilities/');
+define('ROOM_IMG_PATH', SITE_URL . 'images/rooms/'); 
 
 
 
@@ -16,6 +17,7 @@ define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/Hotel-Management-Websi
 define('ABOUT_FLODER', 'about/');
 define('CAROUSEL_FLODER', 'carousel/');
 define('FACILITIES_FLODER', 'facilities/');
+define('ROOM_FLODER', 'rooms/');
 
 // define('ROOM_FLO', '');
 
