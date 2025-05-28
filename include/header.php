fae7bc48e32f359e1ca0c1d9aa5afe88faa20c75
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-28 18:56:45 +0530

    register form Name giving

--- include/header.php
+++ include/header.php
@@ -86,7 +86,7 @@
     aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
-        <form action="">
+        <form id="register-form">
           <div class="modal-header">
             <h5 class="modal-title d-flex aligns-items-center">
               <i class="bi bi-person-lines-fill fs-3 me-2"></i>User Register
@@ -102,39 +102,39 @@
               <div class="row">
                 <div class="col-md-6 ps-0 mb-3">
                   <label class="form-label">Name </label>
-                  <input type="text" class="form-control shadow-none">
+                  <input name="name" type="text" class="form-control shadow-none" required>
                 </div>
                 <div class="col-md-6 p-0 mb-3">
                   <label class="form-label">Email</label>
-                  <input type="email" class="form-control shadow-none">
+                  <input name="email" type="email" class="form-control shadow-none" required>
                 </div>
                 <div class="col-md-6 ps-0 mb-3">
                   <label class="form-label">Phone Number </label>
-                  <input type="number" class="form-control shadow-none">
+                  <input name="phonenum" type="number" class="form-control shadow-none" required>
                 </div>
                 <div class="col-md-6 p-0 mb-3">
                   <label class="form-label">Picture</label>
-                  <input type="file" class="form-control shadow-none">
+                  <input name="profile" type="file" class="form-control shadow-none" accept=".jpg .png .jpeg .webp" required >
                 </div>
                 <div class="col-md-12 p-0 mb-3">
                   <label class="form-label">Address</label>
-                  <textarea class="form-control shadow-none" rows="1"></textarea>
+                  <textarea name="address" class="form-control shadow-none" rows="1"></textarea>
                 </div>
                 <div class="col-md-6 ps-0 mb-3">
                   <label class="form-label">Pincode </label>
-                  <input type="number" class="form-control shadow-none">
+                  <input name="pincode" type="number" class="form-control shadow-none" required>
                 </div>
                 <div class="col-md-6 p-0 mb-3">
                   <label class="form-label">Date of birth</label>
-                  <input type="date" class="form-control shadow-none">
+                  <input name="dob" type="date" class="form-control shadow-none" required>
                 </div>
                 <div class="col-md-6 ps-0 mb-3">
                   <label class="form-label">Password </label>
-                  <input type="password" class="form-control shadow-none">
+                  <input name="pass" type="password" class="form-control shadow-none" required>
                 </div>
                 <div class="col-md-6 p-0 mb-3">
                   <label class="form-label">Confirm Password</label>
-                  <input type="password" class="form-control shadow-none">
+                  <input name="cpass" type="password" class="form-control shadow-none"  required>
                 </div>
               </div>
             </div>
