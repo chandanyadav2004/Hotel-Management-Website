e25d7c44877a70a8587d8aa1eb00f2bc85dce62b
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-29 14:20:56 +0530

    title dynamic links

--- include/links.php
+++ include/links.php
@@ -2,3 +2,17 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap">
 <link rel="stylesheet" href="CSS/common.css">
+
+<?php require('admin/inc/essentials.php') ?>
+<?php require('admin/inc/db_config.php') ?>
+<?php
+$contact_q = "SELECT * FROM `contact_details` where `sr_no`=?";
+$setting_q = "SELECT * FROM `setting` where `sr_no`=?";
+
+$values = [1];
+$contact_res = mysqli_fetch_assoc(select($contact_q, $values, 'i'));
+$setting_res = mysqli_fetch_assoc(select($setting_q, $values, 'i'));
+
+// print_r($contact_res);
+
+?>
