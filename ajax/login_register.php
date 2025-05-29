cd5212adab19fcf56e9a357765ffb47cd71dc78f
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-29 16:33:42 +0530

    login ajax

--- ajax/login_register.php
+++ ajax/login_register.php
@@ -6,9 +6,9 @@ require('../admin/inc/db_config.php');
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require ('../include/PHPMailer-master/PHPMailer-master/src/Exception.php');
-require ('../include/PHPMailer-master/PHPMailer-master/src/PHPMailer.php');
-require ('../include/PHPMailer-master/PHPMailer-master/src/SMTP.php');
+require('../include/PHPMailer-master/PHPMailer-master/src/Exception.php');
+require('../include/PHPMailer-master/PHPMailer-master/src/PHPMailer.php');
+require('../include/PHPMailer-master/PHPMailer-master/src/SMTP.php');
 
 
 
@@ -36,7 +36,7 @@ function sendMail($email, $name, $token)
         $mail->Subject = 'Email Verification Link';
         $mail->Body = "Hi $name,<br><br>
             Click below to verify your email:<br>
-            <a href='".SITE_URL."email_confirm.php?email_confirmation&email=$email&token=$token"."'>Verify Email</a><br><br>
+            <a href='" . SITE_URL . "email_confirm.php?email_confirmation&email=$email&token=$token" . "'>Verify Email</a><br><br>
             If you didn’t register, ignore this email.";
 
         $mail->send();
@@ -80,22 +80,22 @@ if (isset($_POST['register'])) {
     // Send confirmation link to users email
 
     $token = bin2hex(random_bytes(16));
-    $res_mail=sendMail($data['email'],$data['name'],$token);
+    $res_mail = sendMail($data['email'], $data['name'], $token);
 
-    if($res_mail == 0){
+    if ($res_mail == 0) {
         echo "mail_failed";
         exit;
     }
 
-    $enc_pass = password_hash($data['pass'],PASSWORD_BCRYPT);
+    $enc_pass = password_hash($data['pass'], PASSWORD_BCRYPT);
     $q = "INSERT INTO `users_cred`(`name`, `email`, `phonenum`, `profile`, `address`, `pincode`, `dob`, `password`, `token`) 
         VALUES (?,?,?,?,?,?,?,?,?)";
 
-    $values=[$data['name'],$data['email'],$data['phonenum'],$img,$data['address'],$data['pincode'],$data['dob'],$enc_pass,$token];
+    $values = [$data['name'], $data['email'], $data['phonenum'], $img, $data['address'], $data['pincode'], $data['dob'], $enc_pass, $token];
 
-    if(insert($q,$values,'sssssssss')){
+    if (insert($q, $values, 'sssssssss')) {
         echo 1;
-    }else{
+    } else {
         echo 'ins_failed';
     }
 
@@ -103,5 +103,40 @@ if (isset($_POST['register'])) {
 
 }
 
+if (isset($_POST['login'])) {
+    $data = filteration($_POST);
+
+    $u_exist = select(
+        'SELECT * FROM `users_cred` WHERE `email`=?  OR `phonenum`=? LIMIT 1',
+        [$data['email_mob'], $data['email_mob']],
+        'ss'
+    );
+
+    if (mysqli_num_rows($u_exist) == 0) {
+        echo 'inv_email_mob';
+        exit;
+    } else {
+        $u_exist_fetch = mysqli_fetch_assoc($u_exist);
+        if ($u_exist_fetch['is_verified'] == 0) {
+            echo 'not_verified';
+        } else if ($u_exist_fetch['status'] == 0) {
+            echo 'inactive';
+        } else {
+            if (!password_verify($data['pass'], $u_exist_fetch['password'])) {
+                echo 'invalid_pass';
+            } else {
+                session_start();
+                $_SESSION['login'] = true;
+                $_SESSION['uId'] = $u_exist_fetch['id'];
+                $_SESSION['uName'] = $u_exist_fetch['name'];
+                $_SESSION['uPic'] = $u_exist_fetch['profile'];
+                $_SESSION['uPhone'] = $u_exist_fetch['phonenum'];
+                echo 1;
+            }
+
+        }
+
+    }
+}
 
 ?>
