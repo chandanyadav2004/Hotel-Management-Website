233bb812d8ec89cf56c9d9baa98bb0e0146023c3
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-29 16:33:55 +0530

    login

--- include/header.php
+++ include/header.php
@@ -1,8 +1,7 @@
-
 <!-- Nav Bar  -->
 <nav id="nav-bar" class="navbar navbar-expand-lg navbar-light bg-white ps-lg-3 py-lg-2 shadow-sm sticky-top">
   <div class="container-fluid">
-    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php"><?php echo  $setting_res['site_title'] ?></a>
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php"><?php echo $setting_res['site_title'] ?></a>
     <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
       data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
       aria-label="Toggle navigation">
@@ -29,11 +28,33 @@
 
       </ul>
       <div class="d-flex">
-        <!-- <button class="btn btn-outline-success " type="submit">Search</button> -->
-        <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal"
-          data-bs-target="#loginModal">Login</button>
-        <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal"
-          data-bs-target="#registerModal">Register</button>
+        <?php
+        if (isset($_SESSION['login']) && $_SESSION['login'] == true) {
+          $path = USERS_IMG_PATH;
+          echo <<<data
+              <div class="btn-group">
+                <button type="button" class="btn btn-outline-dark shadow-none dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                  <img src="$path$_SESSION[uPic]" style="width: 25px; height:25px;" class="me-1">
+                  $_SESSION[uName]
+
+                </button>
+                <ul class="dropdown-menu dropdown-menu-lg-end">
+                  <li><a class="dropdown-item" href="profile.php">Profile</a></li>
+                  <li><a class="dropdown-item" href="bookings.php">Bookings</a></li>
+                  <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                </ul>
+              </div>
+            data;
+        }
+        else
+        {
+          echo<<<data
+            <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal"data-bs-target="#loginModal">Login</button>
+            <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
+          data;
+        }
+        ?>
+        
       </div>
     </div>
   </div>
@@ -43,7 +64,7 @@
   aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <form action="">
+      <form id="login-form">
         <div class="modal-header">
           <h5 class="modal-title d-flex aligns-items-center">
             <i class="bi bi-person-circle fs-3 me-2"></i>User Login
@@ -52,12 +73,12 @@
         </div>
         <div class="modal-body">
           <div class="mb-3">
-            <label class="form-label">Email address</label>
-            <input type="email" class="form-control shadow-none">
+            <label class="form-label">Email / Mobile</label>
+            <input type="text" name="email_mob" required class="form-control shadow-none">
           </div>
           <div class="mb-4">
             <label class="form-label">Password</label>
-            <input type="password" class="form-control shadow-none">
+            <input type="password" name="pass" class="form-control shadow-none">
           </div>
           <div class="d-flex align-items-center justify-content-between mb-2">
             <button type="submit" class="btn btn-dark shadow-none ">Login</button>
