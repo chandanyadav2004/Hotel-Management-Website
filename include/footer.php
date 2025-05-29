08bd9be367f39806935912a2dbd911d783bf77b3
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-29 13:41:41 +0530

    footer email ajax

--- include/footer.php
+++ include/footer.php
@@ -1,28 +1,26 @@
-
-
-
 <div class="container-fluid bg-white mt-5">
-      <div class="row">
-        <div class="col-lg-4 p-4">
-          <h3 class="h-font fw-bold fs-3 mb-2">Chandan Hotel</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Rem molestiae obcaecati odio delectus cum commodi, ipsa iure voluptatum assumenda repellat beatae sapiente perspiciatis aut omnis quae? Molestiae minima nisi vero.
-          </p>
-        </div>
-        <div class="col-lg-4 p-4">
-          <h5 class="mb-3">Links</h5>
-          <a href="index.php" class="d-inline-block mb-3 text-dark text-decoration-none">Home</a><br>
-          <a href="rooms.php" class="d-inline-block mb-3 text-dark text-decoration-none">Rooms</a><br>
-          <a href="facilities.php" class="d-inline-block mb-3 text-dark text-decoration-none">Facilities</a><br>
-          <a href="contact.php" class="d-inline-block mb-3 text-dark text-decoration-none">Contact us</a><br>
-          <a href="about.php" class="d-inline-block mb-3 text-dark text-decoration-none">About</a><br>
-        </div>
-        <div class="col-lg-4 p-4">
-          <h5 class="mb-3">Follow us</h5>
-          <?php
-          if ($contact_res['tw'] != '') {
-            echo<<<data
+  <div class="row">
+    <div class="col-lg-4 p-4">
+      <h3 class="h-font fw-bold fs-3 mb-2">Chandan Hotel</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Rem molestiae obcaecati odio delectus cum commodi, ipsa iure voluptatum assumenda repellat beatae sapiente
+        perspiciatis aut omnis quae? Molestiae minima nisi vero.
+      </p>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Links</h5>
+      <a href="index.php" class="d-inline-block mb-3 text-dark text-decoration-none">Home</a><br>
+      <a href="rooms.php" class="d-inline-block mb-3 text-dark text-decoration-none">Rooms</a><br>
+      <a href="facilities.php" class="d-inline-block mb-3 text-dark text-decoration-none">Facilities</a><br>
+      <a href="contact.php" class="d-inline-block mb-3 text-dark text-decoration-none">Contact us</a><br>
+      <a href="about.php" class="d-inline-block mb-3 text-dark text-decoration-none">About</a><br>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Follow us</h5>
+      <?php
+      if ($contact_res['tw'] != '') {
+        echo <<<data
             
               <a href="$contact_res[tw]" class="d-inline-block mb-3 ">
                 <span class="badge fs-6 p-2 bg-light text-dark">
@@ -32,10 +30,10 @@
               <br>
             
             data;
-          }
+      }
 
-          if ($contact_res['fb'] != '') {
-            echo<<<data
+      if ($contact_res['fb'] != '') {
+        echo <<<data
             
               <a href="$contact_res[fb]" class="d-inline-block mb-3 ">
                 <span class="badge fs-6 p-2 bg-light text-dark">
@@ -45,9 +43,9 @@
               <br>
             
             data;
-          }
-          if ($contact_res['insta'] != '') {
-            echo<<<data
+      }
+      if ($contact_res['insta'] != '') {
+        echo <<<data
             
               <a href="$contact_res[insta]" class="d-inline-block mb-3 ">
                 <span class="badge fs-6 p-2 bg-light text-dark">
@@ -57,20 +55,42 @@
               <br>
             
             data;
-          }
-          
-          ?>
-        </div>
-      </div>
+      }
+
+      ?>
     </div>
+  </div>
+</div>
 
-    <h6 class="text-center bg-dark  text-white p-3 m-0 ">Designed and Developed by Chandan WEBDEV</h6>
+<h6 class="text-center bg-dark  text-white p-3 m-0 ">Designed and Developed by Chandan WEBDEV</h6>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-      crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <script>
+  function alert(type, msg, position = 'body') {
+    let bs_class = (type == "success") ? "alert-success" : "alert-danger";
+    let element = document.createElement('div');
+    element.innerHTML = `
+                <div class="alert ${bs_class} alert-dismissible fade show " role="alert">
+                    <strong class="me-3">${msg}</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `;
+    if (position == 'body') {
+      document.body.appendChild(element);
+      element.classList.add('custom-alert')
+    } else {
+      document.getElementById(position).appendChild(element);
+    }
+    setTimeout(remAlert, 3000);
+  }
+
+  function remAlert() {
+    document.getElementsByClassName('alert')[0].remove();
+  }
+
+
   function setActive() {
     let navbar = document.getElementById('nav-bar');
     let a_tags = navbar.getElementsByTagName('a');
@@ -80,12 +100,63 @@
       let link_url = a_tags[i].href.split('/').pop();
       let file_name = link_url.split('.')[0];
 
-      if (document.location.href.indexOf(file_name) >=0) {
+      if (document.location.href.indexOf(file_name) >= 0) {
         a_tags[i].classList.add('active');
       } else {
         a_tags[i].classList.remove('active');
       }
-    }       
+    }
   }
-  window.onload = setActive;
+
+  let register_form = document.getElementById('register-form');
+  register_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let data = new FormData();
+
+    data.append('name', register_form.elements['name'].value);
+    data.append('email', register_form.elements['email'].value);
+    data.append('phonenum', register_form.elements['phonenum'].value);
+    data.append('address', register_form.elements['address'].value);
+    data.append('pincode', register_form.elements['pincode'].value);
+    data.append('dob', register_form.elements['dob'].value);
+    data.append('pass', register_form.elements['pass'].value);
+    data.append('cpass', register_form.elements['cpass'].value);
+    data.append('profile', register_form.elements['profile'].files[0]);
+    data.append('register', '');
+
+    var myModal = document.getElementById('registerModal');
+    var modal = bootstrap.Modal.getInstance(myModal);
+    modal.hide();
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "ajax/login_register.php", true);
+    xhr.onload = function () {
+      if (this.responseText == 'pass_mismatch') {
+        alert('error', 'Password Is not Match with confirm password');
+      }else if (this.responseText == 'invalid_image') {
+        alert('error', 'Invalid Image TYpe JPG,PNG,WEBP, JPEG allows');
+      }else if (this.responseText == 'Upd_Failed') {
+        alert('error', 'Upload Failed');
+      }else if (this.responseText == 'email_already') {
+        alert('error', ' email already present');
+      }else if (this.responseText == 'phone_already') {
+        alert('error', ' phone already present');
+      }else if (this.responseText == 'mail_failed') {
+        alert('error', ' cannot sent confirmation mail');
+      }else if (this.responseText == 'ins_failed') {
+        alert('error', ' Insertion Failed ');
+      }else{
+        alert('success','Registration succcessfully');
+        register_form.reset();
+      }
+
+    };
+    xhr.send(data);
+
+  });
+
+  setActive();
+
+
 </script>
