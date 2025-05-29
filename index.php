1083269859b53ceee8f6d34ab544083907b75d53
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-29 14:20:29 +0530

    title dynamic index

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hotel Management Website - Home</title>
   <?php require('include/links.php'); ?>
+  <title><?php echo $setting_res['site_title']; ?> - Home</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
