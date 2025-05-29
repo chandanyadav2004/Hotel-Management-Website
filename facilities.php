c943707ee690722d1b9bd4a07e4d322f2762cde0
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-29 14:19:36 +0530

    title dynamic facilities

--- facilities.php
+++ facilities.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hotel Management Website - Facilities </title>
   <?php require('include/links.php'); ?>
+  <title><?php echo $setting_res['site_title']; ?> - Facilities </title>
 
   
   <style>
