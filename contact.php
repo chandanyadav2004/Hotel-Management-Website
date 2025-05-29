da298bd442cfdbb100c43584d6158ba1fd09c7be
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-29 14:19:50 +0530

    title dynamic contact

--- contact.php
+++ contact.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hotel Management Website - CONTACT </title>
   <?php require('include/links.php'); ?>
+  <title><?php echo $setting_res['site_title']; ?> - CONTACT </title>
 
 
   <style>
