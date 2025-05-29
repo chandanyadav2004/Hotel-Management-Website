0093320df249abfc351d906922472d1ad830f761
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-29 16:33:49 +0530

    login

--- include/footer.php
+++ include/footer.php
@@ -161,6 +161,51 @@
 
   });
 
+
+  let login_form = document.getElementById('login-form');
+  login_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let data = new FormData();
+
+    data.append('email_mob', login_form.elements['email_mob'].value);
+    data.append('pass', login_form.elements['pass'].value);
+    data.append('login', '');
+
+    var myModal = document.getElementById('loginModal');
+    var modal = bootstrap.Modal.getInstance(myModal);
+    modal.hide();
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "ajax/login_register.php", true);
+    xhr.onload = function () {
+      console.log('Server Response:', this.responseText);
+
+      let res = this.responseText.trim();
+
+      if (res == 'inv_email_mob') {
+        alert('error', 'Invalid Mobile No and email');
+      }
+      else if (res == 'not_verified') {
+        alert('error', 'Email Not Verified');
+      }
+       else if (res == 'inactive') {
+        alert('error', 'Admin Blocked contact admin');
+      } 
+      else if (res == 'invalid_pass') {
+        alert('error', 'Invalid Password');
+      }
+      else{
+        window.location = window.location.pathname;
+      } 
+    };
+
+    xhr.send(data);
+
+  });
+
+
+
   setActive();
 
 
