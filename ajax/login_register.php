b933ca1ee4ce9d4b1276a35410955b853f7fb3fd
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-30 12:25:38 +0530

    login register

--- ajax/login_register.php
+++ ajax/login_register.php
@@ -2,6 +2,8 @@
 
 require('../admin/inc/essentials.php');
 require('../admin/inc/db_config.php');
+date_default_timezone_set("Asia/Kolkata");
+
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
