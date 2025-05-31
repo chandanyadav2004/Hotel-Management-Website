e02872dbb75d4f85f305d36ef1f81624b1c090b2
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-31 12:21:28 +0530

    new booking admin

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,44 +1,66 @@
 <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
-        <h3 class="mb-0 h-font">HB WEBSITE</h3>
-        <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
-    </div>
+    <h3 class="mb-0 h-font">HB WEBSITE</h3>
+    <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+</div>
 
-    <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid flex-lg-column align-items-stretch">
-                <h4 class="mt-2 text-light">ADMIN PANEL</h4>
-                <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#adminDropdown" aria-controls="navbarNav" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse flex-column mt-2 align-items-stretch" id="adminDropdown">
-                    <ul class="nav nav-pills flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link text-white " href="dashboard.php">Dashboard</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white " href="users.php">Users</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white " href="user_queries.php">Users Queries</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white " href="rooms.php">Rooms</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white " href="features_facilities.php">Features & Facilities</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white " href="carousel.php">Carousel</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white " href="setting.php">Setting</a>
-                        </li>
-                    </ul>
+<div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid flex-lg-column align-items-stretch">
+            <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
+                data-bs-target="#adminDropdown" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse flex-column mt-2 align-items-stretch" id="adminDropdown">
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <button
+                            class="btn text-white px-3 w-100 shadow-none text-start d-flex align-items-center justify-content-between"
+                            type="button" data-bs-toggle="collapse" data-bs-target="#bookingLinks">
+                            <span>Bookings</span>
+                            <span><i class="bi bi-caret-down-fill"></i></span>
+                        </button>
+                        <div class="collapse show px-3 small mb-1" id="bookingLinks">
+                            <ul class="nav nav-pills flex-column rounded border border-secondary">
+                                
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="new_booking.php">New Booking</a>
+                                </li>
+                                
+                                
+                            </ul>
+
+                        </div>
+
+                    </li>
 
 
-                </div>
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="users.php">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="user_queries.php">Users Queries</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="rooms.php">Rooms</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="features_facilities.php">Features & Facilities</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="carousel.php">Carousel</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="setting.php">Setting</a>
+                    </li>
+                </ul>
+
+
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
+</div>
