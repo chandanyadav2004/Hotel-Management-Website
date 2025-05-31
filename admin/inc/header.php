c97dc88f41b93017f04fe0725af51ef9d018ebda
chandanyadav2004 <demo8788151949@gmail.com>
2025-05-31 12:43:05 +0530

    refund booking

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -30,6 +30,9 @@
                                 <li class="nav-item">
                                     <a class="nav-link text-white" href="new_booking.php">New Booking</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="refund_booking.php">Refund Booking</a>
+                                </li>
                                 
                                 
                             </ul>
