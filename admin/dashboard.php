6e41dd91b87a36bf03d33fe57efd7b8c6d114f40
chandanyadav2004 <demo8788151949@gmail.com>
2025-06-02 12:25:12 +0530

    dyanamic started

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -22,7 +22,34 @@ adminLogin();
 
 <body class="bg-light">
 
-    <?php require('inc/header.php') ?>
+    <?php 
+        require('inc/header.php');
+
+        $is_shutdown = mysqli_fetch_assoc(mysqli_query($con,"SELECT `shutdown` FROM `setting`"));
+
+        $current_bookings = mysqli_fetch_assoc(mysqli_query($con,"SELECT 
+        COUNT(CASE WHEN booking_status='booked' AND arrival=0 THEN 1 END) AS `new_bookings` ,
+        COUNT(CASE WHEN booking_status='cancelled' AND refund=0 THEN 1 END) AS `refund_bookings` 
+        FROM `booking_order`"));
+
+        
+        
+        $unread_queries = mysqli_fetch_assoc(mysqli_query($con,"SELECT COUNT(sr_no) AS `count` 
+        FROM `user_queries` WHERE `seen`=0"));
+
+        $unread_reviews = mysqli_fetch_assoc(mysqli_query($con,"SELECT COUNT(sr_no) AS `count` 
+        FROM `rate_review` WHERE `seen`=0"));
+
+        $current_users = mysqli_fetch_assoc(mysqli_query($con,"SELECT 
+        COUNT(`id`) AS `total`,
+        COUNT(CASE WHEN `status`=1 THEN 1 END) AS `active` ,
+        COUNT(CASE WHEN `status`=0 THEN 1 END) AS `inactive` ,
+        COUNT(CASE WHEN `is_verified`=0 THEN 1 END) AS `unverified` 
+        FROM `users_cred`"));
+
+
+        
+    ?>
 
     <div class="container-fluid" id="main-content">
         <div class="row">
@@ -31,14 +58,24 @@ adminLogin();
 
                 <div class="d-flex align-items-center justify-content-between mb-4">
                     <h3>DASHBOARD</h3>
-                    <h6 class="badge bg-danger py-2 px-2 rounded">Shutdown Mode is Active!</h6>
+                    <?php 
+
+                        if($is_shutdown['shutdown'])
+                        {
+                            echo<<<data
+                            <h6 class="badge bg-danger py-2 px-2 rounded">Shutdown Mode is Active!</h6>
+                            data;
+
+                        }
+                        ?>
+                        
                 </div>
                 <div class="row mb-4">
                     <div class="col-md-3 mb-4">
                         <a href="new_booking.php" class="text-decoration-none">
                             <div class="card text-center text-success p-3">
                                 <h6>New Bookings</h6>
-                                <h1 class="mt-2 mb-0">5</h1>
+                                <h1 class="mt-2 mb-0"><?php echo $current_bookings['new_bookings']; ?></h1>
                             </div>
                         </a>
 
@@ -47,7 +84,7 @@ adminLogin();
                         <a href="refund_booking.php" class="text-decoration-none">
                             <div class="card text-center text-warning p-3">
                                 <h6>Refund Bookings</h6>
-                                <h1 class="mt-2 mb-0">5</h1>
+                                <h1 class="mt-2 mb-0"><?php echo $current_bookings['refund_bookings']; ?></h1>
                             </div>
                         </a>
                     </div>
@@ -55,7 +92,7 @@ adminLogin();
                         <a href="user_queries.php" class="text-decoration-none">
                             <div class="card text-center text-info p-3">
                                 <h6>User Queries</h6>
-                                <h1 class="mt-2 mb-0">5</h1>
+                                <h1 class="mt-2 mb-0"><?php echo $unread_queries['count']; ?></h1>
                             </div>
                         </a>
                     </div>
@@ -63,7 +100,7 @@ adminLogin();
                         <a href="rate_review.php" class="text-decoration-none">
                             <div class="card text-center text-info p-3">
                                 <h6>Rating & Reviews</h6>
-                                <h1 class="mt-2 mb-0">5</h1>
+                                <h1 class="mt-2 mb-0"><?php echo $unread_reviews['count']; ?></h1>
                             </div>
                         </a>
                     </div>
@@ -137,6 +174,38 @@ adminLogin();
                         </div>    
                     </div>
                 </div>
+
+                <h5>Users</h5>
+                <div class="row mb-3">
+                    <div class="col-md-3 mb-4">
+                        <div class="card text-center text-info p-3">
+                            <h6>Total  </h6>
+                            <h1 class="mt-2 mb-0"><?php echo $current_users['total']; ?></h1>
+                        </div>    
+                    </div>
+
+                    <div class="col-md-3 mb-4">
+                        <div class="card text-center text-success p-3">
+                            <h6>Active </h6>
+                            <h1 class="mt-2 mb-0"><?php echo $current_users['active']; ?></h1>
+                        </div>    
+                    </div>
+
+                    <div class="col-md-3 mb-4">
+                        <div class="card text-center text-warning p-3">
+                            <h6>Inactive  </h6>
+                            <h1 class="mt-2 mb-0"><?php echo $current_users['inactive']; ?></h1>
+                        </div>    
+                    </div>
+
+                    <div class="col-md-3 mb-4">
+                        <div class="card text-center text-danger p-3">
+                            <h6>Unverified  </h6>
+                            <h1 class="mt-2 mb-0"><?php echo $current_users['unverified']; ?></h1>
+                        </div>    
+                    </div>
+                </div>
+
 
             </div>
         </div>
